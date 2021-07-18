ea1e4e999aa8a7f8216c43bda6c36a1ee4eb2f60
Jeong-EuiJae <47394504+cnpcnp99@users.noreply.github.com>
2021-07-19 01:36:45 +0900

    처음 저장소에 올림

---
+++ models/Users.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose')
+
+const userSchema = mongoose.Schema({
+    name: {
+        type: String,
+        maxlength: 50
+    },
+    email: {
+        type: String,
+        trim: true,
+        unique: 1
+    },
+    lastname: {
+        type: String,
+        maxlength: 50
+    },
+    role: { // 0: 일반 직원, 1: 임원 같은 식으로 적용할 예정
+        type: Number,
+        default: 0
+    },
+    image: String,
+    token: {
+        type: String
+    },
+    tokenExp: {
+        type: Number
+    }
+})
+
+const User = mongoose.model('User', userSchema)
+
+module.exports = { User }
